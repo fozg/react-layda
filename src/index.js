ec72dc284fdf246dad3a73a875b8250fc9ca0b9e
Fozg <phongdaohong@gmail.com>
2019-01-02 10:20:18 +0700

    remove prop-types\

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
 import Dashboard from './components/Dashboard'
 import LinkToSidebar from './components/LinkToSidebar'
 
 export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
-  }
 
   render() {
     return (<Dashboard {...this.props} />)
